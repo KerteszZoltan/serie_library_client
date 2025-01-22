1001c4af69d64d3328e85f6fffc7e897fe2a4c27
ZoltanKertesz <kertesz.zoltan.9718@gmail.com>
2025-01-22 23:40:15 +0100

    main page basic upgrade which solve the text overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,58 +5,21 @@ export default function Home() {
     <div className={styles.page}>
       <div className={styles.page_background}>
         <div className={styles.page_background_container}>
-          <span>English title:</span>
-          <span>ASD EN TITLE</span>
-          <span>Hungarian title:</span>
-          <span>ASD HUN TITLE</span>
-          <span>English description:</span>
-          <span>ASD EN DESC</span>
-          <span>Hungarian description:</span>
-          <span>ASD HUN DESC
-          </span>
-        </div>
-      </div>
-      <div className={styles.page_background}>
-        <div className={styles.page_background_container}>
-          <span>English title:</span>
-          <span>ASD EN TITLE</span>
-          <span>Hungarian title:</span>
-          <span>ASD HUN TITLE</span>
-          <span>English description:</span>
-          <span>ASD EN DESC</span>
-          <span>Hungarian description:</span>
-          <span>ASD HUN DESC
-          </span>
-        </div>
-      </div>
-      <div className={styles.page_background}>
-        <div className={styles.page_background_container}>
-          <span>English title:</span>
-          <span>ASD EN TITLE</span>
-          <span>Hungarian title:</span>
-          <span>ASD HUN TITLE</span>
-          <span>English description:</span>
-          <span>ASD EN DESC</span>
-          <span>Hungarian description:</span>
-          <span>ASD HUN DESC
-          </span>
-        </div>
-      </div>
-      <div className={styles.page_background}>
-        <div className={styles.page_background_container}>
-          <span>English title:</span>
-          <span>ASD EN TITLE</span>
-          <span>Hungarian title:</span>
-          <span>ASD HUN TITLE</span>
-          <span>English description:</span>
-          <span>ASD EN DESC
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, magni dolorem odio tempora fugit ullam laborum, voluptatem maiores mollitia illo officiis nulla deserunt rerum esse molestiae doloremque modi at reiciendis veniam! Provident velit nihil minus dolorem rerum doloremque consequuntur excepturi dolore et repellat nemo maiores adipisci odio quidem quam, nam accusantium expedita eveniet nesciunt praesentium aut, architecto quibusdam! Perspiciatis non hic consequuntur quam cupiditate libero repellendus tempore totam facilis illo numquam eligendi ipsa voluptatum recusandae, saepe voluptatibus rem. Ipsam fugit est nesciunt autem perferendis possimus, a iure ducimus neque debitis expedita sint natus quisquam sunt tempore animi provident et.
-
-          </span>
-          <span>Hungarian description:</span>
-          <span>ASD HUN DESC
+          <div className={styles.page_background_container_span_container}>
+            <span>English title:</span>
+            <span>ASD EN TITLE</span>
+            <span>Hungarian title:</span>
+            <span>ASD HUN TITLE</span>
+            <span>English description:</span>
+            <span>ASD EN DESC
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, magni dolorem odio tempora fugit ullam laborum, voluptatem maiores mollitia illo officiis nulla deserunt rerum esse molestiae doloremque modi at reiciendis veniam! Provident velit nihil minus dolorem rerum doloremque consequuntur excepturi dolore et repellat nemo maiores adipisci odio quidem quam, nam accusantium expedita eveniet nesciunt praesentium aut, architecto quibusdam! Perspiciatis non hic consequuntur quam cupiditate libero repellendus tempore totam facilis illo numquam eligendi ipsa voluptatum recusandae, saepe voluptatibus rem. Ipsam fugit est nesciunt autem perferendis possimus, a iure ducimus neque debitis expedita sint natus quisquam sunt tempore animi provident et.
-          </span>
+
+            </span>
+            <span>Hungarian description:</span>
+            <span>ASD HUN DESC
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus, magni dolorem odio tempora fugit ullam laborum, voluptatem maiores mollitia illo officiis nulla deserunt rerum esse molestiae doloremque modi at reiciendis veniam! Provident velit nihil minus dolorem rerum doloremque consequuntur excepturi dolore et repellat nemo maiores adipisci odio quidem quam, nam accusantium expedita eveniet nesciunt praesentium aut, architecto quibusdam! Perspiciatis non hic consequuntur quam cupiditate libero repellendus tempore totam facilis illo numquam eligendi ipsa voluptatum recusandae, saepe voluptatibus rem. Ipsam fugit est nesciunt autem perferendis possimus, a iure ducimus neque debitis expedita sint natus quisquam sunt tempore animi provident et.
+            </span>
+          </div>
         </div>
       </div>
       
