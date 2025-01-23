84c0b6a3dde8e4b220435937d88ca8a3bf510179
ZoltanKertesz <kertesz.zoltan.9718@gmail.com>
2025-01-23 19:02:49 +0100

    get all series view in / page

---
+++ src/app/models/IserieModel.ts
@@ -0,0 +1,6 @@
+export default interface SerieProps{
+    titleEN : string,
+    titleHU : string,
+    descriptionEN : string,
+    descriptionHU : string,
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import SerieItem from "@/common/componenets/serieItem/serieItem";
 import styles from "./page.module.scss";
+import { Key } from "react";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('http://localhost:8000/series');
+  const series = await data.json();
   return (
     <div className={styles.page}>
-      <SerieItem></SerieItem>
-      <SerieItem></SerieItem>
-
-      <SerieItem></SerieItem>
-
-      <SerieItem></SerieItem>
-
-      <SerieItem></SerieItem>
-
+      {series.map((series: { _id: Key | null | undefined; titleEN: string; titleHU: string; descriptionEN: string; descriptionHU: string; }) => (
+        <SerieItem key={series._id} titleEN={series.titleEN} titleHU={series.titleHU} descriptionEN={series.descriptionEN} descriptionHU={series.descriptionHU} />
+      ))}
     </div>
   );
 }
